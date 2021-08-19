dd77402b02c6e2cbea7fc0c182fd0cdcfdd40f20
shubhamitradas <shubhamitra@gmail.com>
2021-08-19 11:28:06 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -32,7 +32,7 @@ def load_model(model_name):
 
 def main():
 
-    st.title("Zero Shot Classifier Interpetation Demo App")
+    st.title("Zero Shot Classifier Interpetation Demo App1")
 
     #image = Image.open("./images/tight@1920x_transparent.png")
     #st.sidebar.image(image, use_column_width=True)
