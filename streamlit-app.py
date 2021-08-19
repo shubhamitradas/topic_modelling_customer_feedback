f0d8949e081e9b4fe6015ecb131fd811774169e8
shubhamitradas <shubhamitra@gmail.com>
2021-08-19 12:03:55 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -69,7 +69,7 @@ def main():
     else:
         emb_type_num = 0
         
-    labels = ['technologie','sport','space','medical','historical','graphics','food','entertainment','automation','game','Program','Computer','universe','politics','medicative','ancient','war','Archeology','Paleontologist','visuals','diet','recipe','health','nutrition','movie','TV','Comedy','Opera']  
+    labels = ['technologie','sport'] #'space','medical','historical','graphics','food','entertainment','automation','game','Program','Computer','universe','politics','medicative','ancient','war','Archeology','Paleontologist','visuals','diet','recipe','health','nutrition','movie','TV','Comedy','Opera']  
 
     explanation_classes = ["predicted" + labels]
     explanation_class_choice = st.sidebar.selectbox(
